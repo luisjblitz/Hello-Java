59dd4e08b97f47a6cf608561a5d5db5dfcc7cf0a
luisjblitz <jeronimoquiroz588@gmail.com>
2025-02-13 11:07:39 -0500

    Ejercicio con Math

--- ProyectosNivel1/Main.java
+++ ProyectosNivel1/Main.java
@@ -1,36 +1,23 @@
-import javax.swing.*;
-class IndiceMasaCorporal {
-    double peso;
-    double altura;
-
-    public void solicitarDatos() {
-        String pesoTexto = JOptionPane.showInputDialog(null, "Ingrese su peso en kg...");
-        peso = Double.parseDouble(pesoTexto);
-        String alturaTexto = JOptionPane.showInputDialog(null, "Ingrese su altura en metros...");
-        altura = Double.parseDouble(alturaTexto);
+import java.lang.Math;
+class Geometria {
+    static double calcularAreaCirculo (double radio){
+        return Math.PI * Math.pow(radio, 2);
     }
-
-    public void calculoDeImc() {
-        double imc = peso / Math.pow(altura, 2);
-
-
-        if (imc < 18.5) {
-            JOptionPane.showMessageDialog(null, "Bajo peso , tu IMC es: " + String.format("%.2f",imc));
-        } else if (imc < 24.9) {
-            JOptionPane.showMessageDialog(null, "Peso normal , tu IMC es: " + String.format("%.2f",imc));
-        } else if (imc < 29.9) {
-            JOptionPane.showMessageDialog(null, "Sobrepeso, tu IMC es: " + String.format("%.2f",imc));
-        } else {
-            JOptionPane.showMessageDialog(null, "Obseidad, tu IMC es: " + String.format("%.2f",imc));
-        }
-
+    static double calcularVolumenEsfera (double radio){
+        return (4.0/3.0)*Math.PI * Math.pow(radio, 3);
     }
-
+    static double calcularHipotenusa (double catetoA, double catetoB){
+        return Math.sqrt(Math.pow(catetoA, 2)+ Math.pow(catetoB, 2));
+    }
 }
 public class Main {
-    public static void main (String [] args ){
-        IndiceMasaCorporal persona1 = new IndiceMasaCorporal();
-        persona1.solicitarDatos();
-        persona1.calculoDeImc();
+    public  static void main (String[] args){
+        double areaCirculo = Geometria.calcularAreaCirculo(1.618);
+        double volumenEsfera = Geometria.calcularVolumenEsfera(1.618);
+        double hipotenusa = Geometria.calcularHipotenusa(8.0,5.0);
+
+        System.out.printf("Area del círculo = %.5f%n", areaCirculo);
+        System.out.printf("Volumen esfera = %.2f%n", volumenEsfera);
+        System.out.printf("Hipotenusa = %.2f%n", hipotenusa);
     }
 }
